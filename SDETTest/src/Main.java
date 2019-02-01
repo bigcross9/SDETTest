a98475ee6c2cf0876f2119a7161ec23d53e6a8b4
bigcross9 <bigcross9@gmail.com>
2019-02-01 15:21:33 -0600

    adding test

--- SDETTest/src/Main.java
+++ SDETTest/src/Main.java
@@ -1,9 +1,19 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import org.testng.annotations.Test;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
+	@Test
+	public void main() {
+
+		RestAssured.baseURI = "https://maps.googleapis.com";
+
+		given().param("location", "-33.8670522,151.1957362").param("radius", "500").param("name", "harbour")
+				.param("key", "AIzaSyBTCkTauC4tCfTTLKcjowo-0Y8HWqA4Gfc").when().get("maps/api/place/nearbysearch/json")
+				.then().assertThat().statusCode(200).and().body("results[0].name", equalTo("Harbour Bar & Kitchen"));
+
 	}
 
 }
